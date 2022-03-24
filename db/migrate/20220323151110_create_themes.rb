69f899796626ac57b7496bb582b6d240c0dd93a6
WilfriedPaillot <77113419+WilfriedPaillot@users.noreply.github.com>
2022-03-24 09:58:01 +0100

    Update 20220323151110_create_themes.rb

--- db/migrate/20220323151110_create_themes.rb
+++ db/migrate/20220323151110_create_themes.rb
@@ -3,9 +3,6 @@ class CreateThemes < ActiveRecord::Migration[5.2]
     create_table :themes do |t|
       t.string :title
       t.text :content
-      t.datetime :created_at
-      t.datetime :updated_at
-
       t.timestamps
     end
   end
