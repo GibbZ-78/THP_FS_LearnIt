784001e30d3674a3441af52f879611bfe69f2f77
WilfriedPaillot <77113419+WilfriedPaillot@users.noreply.github.com>
2022-03-24 10:00:06 +0100

    Update 20220323152841_create_question_answers.rb

--- db/migrate/20220323152841_create_question_answers.rb
+++ db/migrate/20220323152841_create_question_answers.rb
@@ -1,11 +1,8 @@
 class CreateQuestionAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :question_answers do |t|
-      t.integer :quiz_id
       t.string :question
       t.string :correct_answer
-      t.datetime :created_at
-      t.datetime :updated_at
       t.belongs_to :quiz, index: true
       t.timestamps
     end
