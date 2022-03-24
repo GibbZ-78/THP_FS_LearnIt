7235842371d693ee36e7748b62bb76e371206ffc
WilfriedPaillot <paillotwilfried@gmail.com>
2022-03-24 14:11:15 +0100

    Updated quiz controller

--- test/controllers/quizzes_controller_test.rb
+++ test/controllers/quizzes_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class QuizesControllerTest < ActionDispatch::IntegrationTest
+class QuizzesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
