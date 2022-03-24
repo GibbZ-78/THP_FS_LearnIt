22cb6f89b27f52a69373edd22491dcdf6d01f9ff
WilfriedPaillot <77113419+WilfriedPaillot@users.noreply.github.com>
2022-03-24 09:58:47 +0100

    Update 20220323152236_create_courses.rb

--- db/migrate/20220323152236_create_courses.rb
+++ db/migrate/20220323152236_create_courses.rb
@@ -1,11 +1,8 @@
 class CreateCourses < ActiveRecord::Migration[5.2]
   def change
     create_table :courses do |t|
-      t.integer :chapter_id
       t.string :title
       t.text :content
-      t.datetime :created_at
-      t.datetime :updated_at
       t.belongs_to :chapter, index: true
       t.timestamps
     end
