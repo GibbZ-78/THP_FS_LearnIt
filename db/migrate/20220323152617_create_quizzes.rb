20431543179d8d36eb3164561fef687aa5494b7f
WilfriedPaillot <77113419+WilfriedPaillot@users.noreply.github.com>
2022-03-24 09:59:35 +0100

    Update 20220323152617_create_quizzes.rb

--- db/migrate/20220323152617_create_quizzes.rb
+++ db/migrate/20220323152617_create_quizzes.rb
@@ -1,10 +1,7 @@
 class CreateQuizzes < ActiveRecord::Migration[5.2]
   def change
     create_table :quizzes do |t|
-      t.integer :course_id
       t.string :title
-      t.datetime :created_at
-      t.datetime :updated_at
       t.belongs_to :course, index: true
       t.timestamps
     end
