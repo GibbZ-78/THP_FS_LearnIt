61f920e9d7cfaf4c94853b327e9e628d162ad395
WilfriedPaillot <77113419+WilfriedPaillot@users.noreply.github.com>
2022-03-24 09:57:19 +0100

    Update 20220323151959_create_chapters.rb

--- db/migrate/20220323151959_create_chapters.rb
+++ db/migrate/20220323151959_create_chapters.rb
@@ -1,11 +1,8 @@
 class CreateChapters < ActiveRecord::Migration[5.2]
   def change
     create_table :chapters do |t|
-      t.integer :theme_id
       t.string :title
       t.text :content
-      t.datetime :created_at
-      t.datetime :updated_at
       t.belongs_to :theme, index: true
       t.timestamps
     end
