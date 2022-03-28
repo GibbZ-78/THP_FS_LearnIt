4e8294ad62b0f5f20abec84fa79727eec205147d
YAROS31 <rochd.yassine@gmail.com>
2022-03-28 23:27:42 +0200

    stripe parameters

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,24 @@
 class StaticPagesController < ApplicationController
   
+  #To be checked
   def home
+    if user_signed_in?
+      if user.role <= 1
+        if membership != nil
+          if membership.timestamps < datetime.now - 365
+            <%= link_to checkout_create_path %>
+          else 
+            <%= link_to dashboard_path %>
+          end
+        else
+          <%= link_to checkout_create_path %>
+        end
+      else
+        <%= link_to dashboard_path %>
+      end
+    else
+      <%= link_to 'Login', ... %> #substitute the ... for whatever path goes to login
+    end
   end
   
   def uikit
