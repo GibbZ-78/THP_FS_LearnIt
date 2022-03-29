fe4b8de2ce3774c585b594bfa56c07c67a00ae4b
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-03-29 02:09:37 +0200

    Nightly backup including: an update of the dashboard controller + my first attempts to create the admin dashboard view + some updates on the User model + updates of the routes for this same admin dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,15 @@ class User < ApplicationRecord
   has_many :user_seasons
   has_many :seasons, through: :user_seasons
 
+  # Return the role of a given "User" object
+  def what_role?
+    return self.role
+  end
+
   private
 
   def set_default_role
-    self.role ||= :user
+    self.role ||= :student
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
 
   devise_for :users
-  resources :dashboard
+  
+  # JBV - Adding specific routes for dashboards' management
+  scope "/dashboard" do
+    get "admin", to: "dashboard#admin", as: "dashboard_admin"   # JBV - Specific route for the ADMIN dashboard
+  end
+  
+  resources :dashboard  # JBV - Not sure it is useful to keep this as we call only non-standard methods from this controller...?
   
   resources :themes
   resources :chapters
