896d8601ce3e943508ed9721314e49c3e25cd2f6
YAROS31 <rochd.yassine@gmail.com>
2022-03-29 20:21:46 +0200

    Stripe after sign in implemented in the application controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,22 +1,6 @@
 class StaticPagesController < ApplicationController
   
   def home
-    if user_signed_in?
-      if current_user.role = 0
-        membership = Membership.where(user_id:current_user.id).last
-        if membership != nil
-          if membership.subscription_date >= datetime.now() - 365.days
-            redirect_to dashboard_index_path, method: "get"
-         else
-            redirect_to checkout_create_path, method: "post"
-          end
-        else
-          redirect_to new_user_registration_path, method: "get"
-        end
-      else
-        redirect_to dashboard_index_path, method: "get"
-      end
-    end # If not logged in, does nothing
   end
   
   def uikit
