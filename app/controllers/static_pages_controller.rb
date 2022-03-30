721f0243cd01e082533dce7e6e47baf9908e21a0
WilfriedPaillot <77113419+WilfriedPaillot@users.noreply.github.com>
2022-03-30 10:35:28 +0200

    Update static_pages_controller.rb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,26 +1,7 @@
 class StaticPagesController < ApplicationController
-  
-  # Invoking DEVISE to benefit from "user_signed_in?" and "current_user" helpers
   before_action :authenticate_user!
 
   def home
-    # if user_signed_in?
-    #   if current_user.what_role? == "student"
-    #     membership = Membership.where(user_id:current_user.id).last
-    #     if !membership.nil?
-    #       if membership.subscription_date >= datetime.now() - 365.days
-    #         redirect_to dashboard_index_path, method: "get"
-    #       else
-    #         redirect_to checkout_create_path, method: "post"
-    #       end
-    #     else
-    #       redirect_to new_user_registration_path, method: "get"
-    #     end
-    #   else
-    #     # TO DO: See with the team if we want "mentor" / "admin" to be force-moved to their dashboard or not?
-    #     # COMMENTED FOR NOW - redirect_to dashboard_index_path, method: "get"
-    #   end
-    # end # If not logged in, does nothing
   end
   
   def uikit
