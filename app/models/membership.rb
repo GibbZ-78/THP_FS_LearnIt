bf9686446febddcf91e624225722734f72c25868
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-03-31 13:11:12 +0200

    Updated the SEED with nullation of completion when related season's start_date is in the future (Time.now) + Recommented membership mail sendings

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,15 +1,15 @@
 class Membership < ApplicationRecord
   belongs_to :user
 
-  #ActionMailer callback after "Order" creation to send the User an email with its kitten picture 
-
-  after_create :membership_send
+  # ActionMailer callback after "Membership" creation to:
+  # - send a "welcome message" to the Student
+  # - inform the Admin a new Student as subscribe a yearly membership
+  #after_create :membership_send
+  #after_create :admin_send
 
   def membership_send
     UserMailer.membership_email(self).deliver_now
   end
-
-  after_create :admin_send
 
   def admin_send
     UserMailer.admin_email(self).deliver_now
