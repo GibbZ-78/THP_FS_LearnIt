d4ff13e02994e6a4076a5141ce833eff2df9735a
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-04-01 02:37:35 +0200

    Updated SEED with more flesh around the courses' bones + Implemented CRUD for 'Theme' object + Doing so, created a multi-purpose partial 'tcc_partial' + Updated admin dashboard with a double accordion giving access to 'Theme' and 'Chapter' objects + ...

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -8,19 +8,34 @@ class ChaptersController < ApplicationController
     @chapter = Chapter.find(params[:id]) 
   end
 
-  #def new
-  #end
+  def new
+    @chapter = Chapter.new
+  end
 
-  #def update
-  #end
+  def create
+    @chapter.create(clean_params)
+  end
 
-  #def new
-  #end
+  def edit
+    @chapter = Chapter.find(params[:id]) 
+  end
 
-  #def create
-  #end
+  def update
+    @chapter = Chapter.find(params[:id])
+    @chapter.update(clean_params)
+    redirect_to dashboard_admin_path
+  end
 
-  #def destroy
-  #end
+  def destroy
+    @chapter = Chapter.find(params[:id])
+    @chapter.destroy
+    redirect_to dashboard_admin_path
+  end
+
+  private
+
+  def clean_params
+    params.require(:chapter).permit(:title, :content)
+  end
 
 end
